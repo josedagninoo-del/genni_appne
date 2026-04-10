7a479f9c7b849053ca79cb00a9e42f67cf030bbe
josedagninoo-del <josedagninoo@gmail.com>
2026-04-10 16:21:50 -0700

    Update Gennie_app.py

--- Gennie_app.py
+++ Gennie_app.py
@@ -668,11 +668,10 @@ st.markdown(f"""
 
 if "GAMBIT" in final_strategy["name"].upper():
 
-    st.markdown("## 🧠 GENIE GAMBIT 2.0 — PLAN PROFESIONAL")
+     st.markdown("## 🧠 GENIE GAMBIT 2.0 — PLAN PROFESIONAL")
 
-    st.markdown("""
-** Strategy Summary**  
-With this gambit, we will be hedging across both match odds and goal market.  
+     st.markdown("""
+ ** Strategy Summary:** With this gambit, we will be hedging across both match odds and goal market.  
 Best scenario is two goals for the favorite but you must be prepared to pivot in other scenarios.
 
 **📊 Market to Trade:**  Match Odds y Over 2.5 Goals  
