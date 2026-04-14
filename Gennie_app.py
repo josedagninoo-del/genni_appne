7625513c7d23e0fbd7593dd257a62c63e72ef1ae
josedagninoo-del <josedagninoo@gmail.com>
2026-04-14 11:12:44 -0700

    Update Gennie_app.py

--- Gennie_app.py
+++ Gennie_app.py
@@ -886,7 +886,7 @@ if stats:
 
 
 ph, pa, goals, xg_h, xg_a, goals_trend, scoring, tactics, strategy, market, entry, exit, confidence = genie_analysis(
-    home, away, row.H, row.D, row.A, attack_factor, over25, under25
+    home, away, row.H, row.D, row.A, attack_factor
 )
 
 label, score = classify_match(ph, pa, goals, row.H)
